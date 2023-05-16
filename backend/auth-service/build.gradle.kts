cc95813cd6eb2066c9f9ec523f8e4c01fd6abbc4
MaximKa99 <pokhodinmv@gmail.com>
2023-05-16 23:16:42 +0600

    init auth-service

--- backend/auth-service/build.gradle.kts
+++ backend/auth-service/build.gradle.kts
@@ -1,22 +1,26 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
+    kotlin("plugin.jpa") version "1.8.0"
+    kotlin("plugin.spring") version "1.8.0"
+    id("org.springframework.boot") version "2.7.2"
+    id("io.spring.dependency-management") version "1.1.0"
+    application
 }
-
-group = "com.pro100byte"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(8)
+application {
+    mainClass.set("com.pro100byte.AppKt")
 }
