e19d8f4e5f62f93a1b1427f7f842541b125df11e
MaximKa99 <pokhodinmv@gmail.com>
2023-05-24 00:05:41 +0600

    fix

--- backend/vacancy-service/build.gradle.kts
+++ backend/vacancy-service/build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
